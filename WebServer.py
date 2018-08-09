beb73ddd0af9acadc1d6ac2bb9d65355ead324d8
dzuro02 <dzuro02@ca.com>
2018-08-09 17:44:50 +0200

    Updated comment regarding running Selenium and Python HTTP server

--- WebServer.py
+++ WebServer.py
@@ -1,10 +1,9 @@
 import http.server
 import socketserver
 
-# Run simple http server that will make content of local directory (where is the script) accessible to web browser
+# Run simple http server that will make content of current directory (location of the python script file) accessible to web browser
 # alternative to this code is run following code from command line:     python -m http.server --cgi 8000
-# It looks that running HTTP server and Selenium at the same time from PyCharm IDE causes port locking error
-# Running webserver from command line and calling Selenium form IDE works fine though.
+
 PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
