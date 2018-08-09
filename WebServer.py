761067d85aefe62b17c0b10b4c48bda8862342fc
dzuro02 <dzuro02@ca.com>
2018-08-09 17:26:54 +0200

    Create module with simple http server as support for selenium driven automation

--- WebServer.py
+++ WebServer.py
@@ -4,7 +4,6 @@ import socketserver
 # Run simple http server that will make content of local directory (where is the script) accessible to web browser
 # alternative to this code is run following code from command line:     python -m http.server --cgi 8000
 PORT = 8000
-
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
